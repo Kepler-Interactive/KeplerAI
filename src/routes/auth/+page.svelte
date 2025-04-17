31cf0e7f225245a2376ec8af0ecc8bf1f888a381
JarrydAnderson-Kepler <115781002+JarrydAnderson-Kepler@users.noreply.github.com>
2025-04-17 18:23:02 +0800

    Update +page.svelte

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -175,7 +175,7 @@
 />
 
 <div class="w-full h-screen max-h-[100dvh] text-white relative">
-	<div class="w-full h-full absolute top-0 left-0 bg-white dark:bg-black"></div>
+	<div class="w-full h-full absolute top-0 left-0" style="background-color: #3834FF;"></div>
 
 	<div class="w-full absolute top-0 left-0 right-0 h-8 drag-region" />
 
